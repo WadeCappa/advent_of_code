4fddccf34a049076a01e6a7983a21fcce09e9792
Wade Cappa <wade.cappa@msn.com>
2024-12-07 22:09:52 -0500

    elixir solution

---
+++ 2024/day_6/solution.ex
@@ -0,0 +1,80 @@
+defmodule Day6 do
+
+  def getStart(map) do
+    map
+      |> Enum.reduce({0, 0}, fn {{y, x}, v}, s ->
+        case v == "^" do
+          true -> {y, x}
+          false -> s
+        end
+      end)
+  end
+
+  def bumpDirection(d) do
+    case d do
+      {-1, 0} -> {0, 1}
+      {0, 1} -> {1, 0}
+      {1, 0} -> {0, -1}
+      {0, -1} -> {-1, 0}
+    end
+  end
+
+  def nextDir(map, {y, x}, {dy, dx}) do
+    case Map.get(map, {y + dy, x + dx}, "X") do
+      "#" -> nextDir(map, {y, x}, bumpDirection({dy, dx}))
+      _ -> {dy, dx}
+    end
+  end
+
+  def takeStep(map, {y, x}, {dy, dx}) do
+    case Map.get(map, {y + dy, x + dx}, "X") do
+      "X" -> :done
+      "#" -> takeStep(map, {y, x}, nextDir(map, {y, x}, {dy, dx}))
+      _ -> {{y + dy, x + dx}, {dy, dx}}
+    end
+  end
+
+  def getPath(map, path, p, d) do
+    case takeStep(map, p, d) do
+      :done -> path
+      {p, d} -> getPath(map, MapSet.put(path, p), p, d)
+    end
+  end
+
+  def checkIfLoop(map, seen, p, d) do
+    if Map.has_key?(seen, {p, d}) do
+      true
+    else
+      case takeStep(map, p, d) do
+        :done -> false
+        {new_p, new_d} -> checkIfLoop(map, Map.put(seen, {p, d}, 0), new_p, new_d)
+      end
+    end
+  end
+
+  def asMap(strings) do
+    String.split(strings, "\n")
+      |> Enum.with_index()
+      |> Enum.map(fn {line, y} ->
+        line
+          |> String.graphemes()
+          |> Enum.with_index()
+          |> Enum.reduce(%{}, fn {v, x}, acc -> Map.put(acc, {y, x}, v) end)
+      end)
+      |> Enum.reduce(%{}, fn line, acc -> Map.merge(line, acc) end)
+  end
+
+  def run(file) do
+    map = case File.read(file) do
+      {:ok, contents} -> asMap(contents)
+    end
+
+    start = getStart(map)
+    path = getPath(map, MapSet.new([start]), start, {-1, 0})
+    IO.puts MapSet.size(path)
+
+    path
+      |> MapSet.filter(fn p -> checkIfLoop(Map.replace(map, p, "#"), %{}, start, {-1, 0}) end)
+      |> MapSet.size()
+  end
+end
