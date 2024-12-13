1fb71147431008ca06bfbebb84a5210583dd9413
Wade Cappa <wade.cappa@msn.com>
2024-12-12 20:49:47 -0500

    elixir day 11 partial solution, no memo yet

---
+++ 2024/day_11/solution.exs
@@ -0,0 +1,51 @@
+defmodule Day11 do
+  def readStones(file) do
+    case File.read(file) do
+      {:ok, stones} -> stones
+        |> String.trim()
+        |> String.split(" ")
+        |> Enum.map(fn x -> String.to_integer(x) end)
+    end
+  end
+
+  def blink(stone) do
+    s = Integer.to_string(stone)
+    l = String.length(s)
+    if stone == 0 do
+      [1]
+    else
+      if rem(l, 2) == 0 do
+        m = div(String.length(s), 2)
+        [String.slice(s, 0..m-1), String.slice(s, m..l)]
+          |> Enum.map(fn x -> String.to_integer(x) end)
+      else
+        [stone * 2024]
+      end
+    end
+  end
+
+  def dfs(stone, its) do
+    if its == 0 do
+      1
+    else
+      blink(stone)
+        |> Enum.map(fn s -> dfs(s, its - 1) end)
+        |> Enum.reduce(fn stones, acc -> stones + acc end)
+    end
+  end
+
+  def countStonesAfterBlinks(stones, its) do
+    stones
+      |> Enum.map(fn s -> dfs(s, its) end)
+      |> Enum.reduce(fn stones, acc -> stones + acc end)
+  end
+end
+
+[file, its | _] = System.argv
+its = String.to_integer(its)
+IO.puts file
+IO.puts its
+
+stones = Day11.readStones(file)
+
+Day11.countStonesAfterBlinks(stones, its) |> IO.inspect()
